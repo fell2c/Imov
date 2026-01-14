4b00b6fd0d59e738c635fa400d80e7ac36dd1e06
Felipe Cieslak <felipecieslak@gmail.com>
2026-01-13 22:12:50 -0300

    * Ajustado V1__create_initial. * Ajustado pom com a versão 3.5.9 do SpringBoot para compatibilidade com Flyway. * Ajustado application.properties.

---
+++ Imov/Imov/src/test/java/nle/co/Imov/ImovApplicationTests.java
@@ -0,0 +1,13 @@
+package nle.co.Imov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
